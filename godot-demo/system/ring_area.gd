da3db7a11f408ea06e63600519b5051214c5bb8f
jozfeal <163903053+jozfeal@users.noreply.github.com>
2026-09-07 15:40:08 -0400

    Added Winning Display
    Text on screen says which robot won the round and applies correct color to text for emphasis. Additionally fixed collision shape for increased platform size.

--- godot-demo/system/ring_area.gd
+++ godot-demo/system/ring_area.gd
@@ -1,10 +1,13 @@
 extends Area3D
 
 @export var robots: Array[Robot]
+@export var win_text: Label
 
 func _on_body_exited(body: Node3D) -> void:
 	if body is Robot:
 		robots.erase(body)
 		
 		if robots.size() == 1:
-			print("%s wins!" % robots[0].robot_name)
+			win_text.label_settings.font_color = robots[0].color
+			win_text.text = "%s wins!" % robots[0].robot_name
+			win_text.show()
