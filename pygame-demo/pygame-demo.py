4d6957a83e722b37d0eec15cbb308ee75e4662fc
jozfeal <163903053+jozfeal@users.noreply.github.com>
2026-09-08 12:13:43 -0400

    Added Game Loop and Ring
    Game now has a proper loop and can be closed out of.  Added a black background with a white ring in the center.

--- pygame-demo/pygame-demo.py
+++ pygame-demo/pygame-demo.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 
 # Pygame Setup
 pygame.init()
@@ -7,3 +8,33 @@ screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
 pygame.display.set_caption("Robot Sumo Wrestling - Pygame Demo")
 clock = pygame.time.Clock()
 FPS = 60
+FONT = pygame.font.SysFont("arial", 48, bold=True)
+
+# Ring properties
+RING_CENTER = (SCREEN_W // 2, SCREEN_H // 2)
+RING_RADIUS = 400
+
+# RGB values for used colors
+WHITE = (255, 255, 255)
+BLACK = (20, 20, 20)
+BLUE = (60, 120, 230)
+RED = (220, 70, 70)
+
+# Main game loop
+running = True
+while running:
+    # Checks for window being closed
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    # Black background with white ring
+    screen.fill(BLACK)
+    pygame.draw.circle(screen, WHITE, RING_CENTER, RING_RADIUS, width=15)
+
+    # Redraws screen and continues game
+    pygame.display.flip()
+    clock.tick(FPS)
+
+pygame.quit()
+sys.exit()
